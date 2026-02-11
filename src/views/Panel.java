c6f79d09e63570d7d20992dd48e2b889cf8f0515
Mrpapita2124 <josecarlos.arcelopez@gmail.com>
2026-02-11 10:02:55 -0700

    mensaje de credenciales

--- src/views/Panel.java
+++ src/views/Panel.java
@@ -29,17 +29,20 @@ public class Panel extends JPanel{
 	{	
 		setLayout(null);
 		
-		JButton button = new JButton("Iniciar Sesión");
 		
-		crearBoton(button, "..\\img\\icono.png");
-		
-		add(button);
 		
 		JLabel saludo = new JLabel("Bienvenido!");
-		saludo.setForeground(textoColor);
+		saludo.setForeground(Color.BLACK);
 		saludo.setFont(fontTitulo);
-		saludo.setBounds(450,200,500, 100);
+		saludo.setBounds(440,220,500, 100);
 		add(saludo);
+		
+		JLabel credenciales = new JLabel("Credenciales erroneas");
+		credenciales.setForeground(Color.RED);
+		credenciales.setFont(fontTexto);
+		credenciales.setBounds(460,290,300, 50);
+		credenciales.setVisible(false);
+		add(credenciales);
 		
 		JTextField usuario = new JTextField();
 		usuario.setForeground(Color.GRAY);
@@ -53,6 +56,14 @@ public class Panel extends JPanel{
 		contraseña.setFont(fontTexto);
 		contraseña.setBounds(525,390,220, 60);
 		add(contraseña);
+		
+		JButton button = new JButton("Iniciar Sesión");
+		crearBoton(button, "..\\img\\icono.png");
+		button.addActionListener(e -> {
+			credenciales.setVisible(true);
+		});
+		add(button);
+		
 	}
 	
 	private void crearBoton(JButton button, String ruta)
@@ -76,5 +87,6 @@ public class Panel extends JPanel{
 			System.out.println("No se pudo poner el icono");
 		}
 	}
+	
 	
 }
